6f6eb709f9980a1fc276818aef086bebab2df443
ismailaliyevv <dostumisi@gmail.com>
2026-03-15 16:14:46 +0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,13 @@
 // ║  console.firebase.google.com → Layihə → Ayarlar  ║
 // ╚══════════════════════════════════════════════════╝
 const firebaseConfig = {
-  apiKey:            "BURAYA_API_KEY",
-  authDomain:        "BURAYA.firebaseapp.com",
-  projectId:         "BURAYA_PROJECT_ID",
-  storageBucket:     "BURAYA.appspot.com",
-  messagingSenderId: "BURAYA_SENDER_ID",
-  appId:             "BURAYA_APP_ID"
+  apiKey: "AIzaSyCfQcSQRzkMEPatTOeJ5on9eVPKZSywbHc",
+  authDomain: "derman-2ba9b.firebaseapp.com",
+  projectId: "derman-2ba9b",
+  storageBucket: "derman-2ba9b.firebasestorage.app",
+  messagingSenderId: "957997391368",
+  appId: "1:957997391368:web:81bc206b88923b8da5bbbd",
+  measurementId: "G-PF5SM3BSR8"
 };
 
 firebase.initializeApp(firebaseConfig);
